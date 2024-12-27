afd4ad326d77b4b4c67b4908bd618e18381bd502
Robert Gemrot <robert.gemrot@centrum.cz>
2024-12-27 18:52:35 +0100

    improve: Exit with non-zero exit code on failure

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
+
 	"github.com/metju-ac/train-me-maybe/internal/handlers"
 	openapiclient "github.com/metju-ac/train-me-maybe/openapi"
-	"time"
 )
 
 func main() {
@@ -16,19 +18,20 @@ func main() {
 	departingStation, arrivingStation, selectedRoutes, err := handlers.HandleRouteSelection(apiClient)
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		os.Exit(1)
 	}
 
 	seatClasses, err := handlers.HandleSeatClassSelection(apiClient)
 	if err != nil {
 		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
 
 	for {
 		freeSeats, err := handlers.CheckFreeSeats(apiClient, departingStation, arrivingStation, selectedRoutes, seatClasses)
 		if err != nil {
 			fmt.Println("Error:", err)
-			return
+			os.Exit(1)
 		}
 
 		if freeSeats {

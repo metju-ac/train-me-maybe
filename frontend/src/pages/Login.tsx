724bd24ad093cf11ebde63f50d9417cb932471a2
Robert Gemrot <robert.gemrot@centrum.cz>
2025-01-02 17:24:36 +0100

    feat: Add login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,3 +1,81 @@
+import { ToastBarContext } from "@components/ToastBarProvider";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import userService from "@services/userService";
+import { useMutation } from "@tanstack/react-query";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router";
+
 export default function Login() {
-    return <h1>Login</h1>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Mutations
+  const mutation = useMutation({
+    mutationKey: [userService.loginUser.key],
+    mutationFn: userService.loginUser.fn,
+  });
+
+  const { setToast } = useContext(ToastBarContext);
+  let navigate = useNavigate();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    // Handle form submission logic here
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    mutation.mutate(
+      { email, password },
+      {
+        onSuccess: (data) => {
+          console.log("User logged in successfully", data);
+          setToast("Successfully logged in", "success");
+          navigate("/");
+        },
+        onError: (error) => {
+          console.error("Error logging in", error);
+          setToast("Error logging in: " + error.message, "error");
+        },
+      }
+    );
+  };
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 2,
+      }}
+    >
+      <Typography variant="h4" component="h1" gutterBottom>
+        Login
+      </Typography>
+      <TextField
+        label="Email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        label="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+        fullWidth
+        margin="normal"
+      />
+      <Button type="submit" variant="contained" color="primary" sx={{ mt: 2 }}>
+        Login
+      </Button>
+    </Box>
+  );
 }

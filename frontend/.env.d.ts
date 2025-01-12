6e60d03dd2ee092ea8478b8325473ca1472a9b2b
Robert Gemrot <robert.gemrot@centrum.cz>
2025-01-12 16:41:50 +0100

    Add email into footer

--- frontend/.env.d.ts
+++ frontend/.env.d.ts
@@ -1,6 +1,7 @@
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: string;
+  readonly VITE_API_BASE_URL: string | undefined;
   readonly VITE_USE_MOCKS: string | undefined;
+  readonly VITE_SUPPORT_EMAIL: string | undefined;
 }
 
 interface ImportMeta {
